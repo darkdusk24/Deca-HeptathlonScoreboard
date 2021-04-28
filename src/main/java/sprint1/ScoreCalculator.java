7e9481073f23cf186b957dbc5011965a94f53ea8
Mikael Byström <mibit@protonmail.com>
2021-04-28 11:02:18 +0200

    Merge av nuvarande main b00da0a och min branch mb-sprint1

---
+++ src/main/java/sprint1/ScoreCalculator.java
@@ -0,0 +1,10 @@
+package sprint1;
+
+public class ScoreCalculator {
+
+	public int OneHundredMeters(double performance) {
+		int score = (int) (25.4347 * Math.pow((18 - performance), 1.81));
+
+		return score;
+	}
+}
