3327d1439c28a6756f15da9e7720c59eed0d4e62
Inha Hara <inhahara@gmail.com>
2021-05-02 21:53:39 +0200

    Sprint1 - add Excel Exporter

---
+++ src/main/java/decaHepta/export/ExcelExporter.java
@@ -0,0 +1,68 @@
+package decaHepta.export;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFFont;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+
+public class ExcelExporter {
+	
+	public static void export(List<String> headers, List<List<Object>> rows) throws IOException {
+        Workbook workbook = new XSSFWorkbook();
+
+        try {
+            Sheet sheet = workbook.createSheet("Scoreboard");
+
+            Row header = sheet.createRow(0);
+            CellStyle headerStyle = workbook.createCellStyle();
+            headerStyle.setFillForegroundColor(IndexedColors.LIGHT_BLUE.getIndex());
+            headerStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+            XSSFFont font = ((XSSFWorkbook) workbook).createFont();
+            font.setFontName("Arial");
+            font.setFontHeightInPoints((short) 16);
+            font.setBold(true);
+            headerStyle.setFont(font);
+            for (int i = 0; i < headers.size(); i++) {
+            	String headerName = headers.get(i);
+            	Cell headerCell = header.createCell(i);
+                headerCell.setCellValue(headerName);
+                headerCell.setCellStyle(headerStyle);
+            }
+ 
+            CellStyle style = workbook.createCellStyle();
+            style.setWrapText(true);
+            
+            for (int i = 0; i < rows.size(); i++) {
+            	List<Object> rowData = rows.get(i);
+            	Row row = sheet.createRow(i + 2);
+            	for (int j = 0; j < rowData.size(); j++) {
+	                Cell cell = row.createCell(j);
+	                cell.setCellValue(rowData.get(j) != null ? rowData.get(j).toString() : "");
+	                cell.setCellStyle(style);
+            	}
+            }
+
+            File currDir = new File(".");
+            String path = currDir.getAbsolutePath();
+            String fileLocation = path.substring(0, path.length() - 1) + "Scoreboard.xlsx";
+
+            FileOutputStream outputStream = new FileOutputStream(fileLocation);
+            workbook.write(outputStream);
+        } finally {
+            if (workbook != null) {
+            	workbook.close();
+               
+            }
+        }
+    }
+}
---
+++ src/test/java/decaHepta/export/ExcelExporterTest.java
@@ -0,0 +1,38 @@
+package decaHepta.export;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+public class ExcelExporterTest {
+
+    @Test
+    public void exportTest() throws IOException {
+    	List<String> headers = new ArrayList<>();
+    	headers.add("First name");
+    	headers.add("Last name");
+    	headers.add("Sport Type");
+    	headers.add("Result");
+    	headers.add("Points");
+    	
+    	List<List<Object>> rowData = new ArrayList<>();
+    	List<Object> competitor1 = new ArrayList<>();
+    	competitor1.add("John");
+    	competitor1.add("Smith");
+    	competitor1.add("100m");
+    	competitor1.add("12s");
+    	competitor1.add("100");
+    	rowData.add(competitor1);
+    	List<Object> competitor2 = new ArrayList<>();
+    	competitor2.add("Mark");
+    	competitor2.add("Dow");
+    	competitor2.add("100m");
+    	competitor2.add("13s");
+    	competitor2.add("90");
+    	rowData.add(competitor2);
+    	
+    	ExcelExporter.export(headers, rowData);
+    }
+}
