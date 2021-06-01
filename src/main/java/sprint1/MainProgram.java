b7d1b6e2faa56a986f47509e1958d0693b45ce1f
Mikael Byström <mibit@protonmail.com>
2021-06-01 12:30:38 +0200

    Main Event Selection

--- src/main/java/sprint1/MainProgram.java
+++ src/main/java/sprint1/MainProgram.java
@@ -14,10 +14,11 @@ public class MainProgram {
 	private static String[] heptathlon = { "100 m hurdles", "High jump", "Shot put", "200 m", "Long jump",
 			"Javelin throw", "800 m" };
 	private static ScoreCalculator calc = new ScoreCalculator();
-	private static String mainEvent = "Heptathlon";
+	private static MainEventType mainEvent = MainEventType.HEPTATHLON;
 
 	public static void main(String[] args) {
 		logIn();
+		mainEventInput();
 		contestantInput();
 		resultInput();
 	}
@@ -34,6 +35,26 @@ public class MainProgram {
 			}
 		}
 	}
+	
+	private static void mainEventInput() {
+		while (true) {
+			System.out.println("Default Main Event is " + mainEvent.eventName 
+			+ ". Type 1 to keep, 2 for Decathlon");
+			String eventSelection = scan.nextLine();
+
+			if (eventSelection.matches("1|h")) {
+			    break;
+			}	
+			if (eventSelection.matches("2|d")) {
+			    mainEvent = MainEventType.DECATHLON;
+			    System.out.println("Main Event is now " + mainEvent.eventName);
+			    break;
+			} else {
+			    System.out.println("Please type 1 for Heptahlon, 2 for Decathlon");
+			}
+		}
+	}
+
 
 	private static void contestantInput() {
 		int contestants = 0;
@@ -67,8 +88,7 @@ public class MainProgram {
 	private static void resultInput() {
 		System.out.println(
 				"The result should be measured in seconds (running), metres (throwing) or centimetres (jumping). \n");
-
-		if (mainEvent.equalsIgnoreCase("Decathlon")) {
+		if ((mainEvent.eventName).equalsIgnoreCase("Decathlon")) {
 			for (int i = 0; i < decathlon.length; i++) {
 				for (int j = 0; j < scoreboard.size(); j++) {
 					System.out.println("Input the result " + scoreboard.get(j).getName() + " got on the " + decathlon[i]
@@ -91,7 +111,7 @@ public class MainProgram {
 					ConsoleOutput.printResultTable(scoreboard);
 				}
 			}
-		} else if (mainEvent.equalsIgnoreCase("Heptathlon")) {
+		} else if ((mainEvent.eventName).equalsIgnoreCase("Heptathlon")) {
 			for (int i = 0; i < heptathlon.length; i++) {
 				for (int j = 0; j < scoreboard.size(); j++) {
 					System.out.println("Input the result " + scoreboard.get(j).getName() + " got on the "
